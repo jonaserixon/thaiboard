fb557c7618040a2943dd2544853c237d62c57ce4
Jonas Erixon <x.jonaz@gmail.com>
2018-09-29 21:40:24 +0200

    implemented capslock and shift

--- thaiboard/src/containers/Keyboard.tsx
+++ thaiboard/src/containers/Keyboard.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import Key from '../components/Key';
 
 interface IKeyboardState {
-    input: string,
+    input: string[],
     isCapsLock: boolean,
-    isSpace: boolean
+    isShift: boolean,
+    keyboard: object[],
 }
 
 class Keyboard extends React.Component<{}, IKeyboardState> {
@@ -12,31 +13,80 @@ class Keyboard extends React.Component<{}, IKeyboardState> {
         super(props);
 
         this.state = {
-            input: '',
+            input: [],
             isCapsLock: false,
-            isSpace: false
+            isShift: false,
+            keyboard: [],
         }
+    }
+
+    public componentDidMount() {
+        console.log('did mount')
+        this.setState({keyboard: this.createKeyboard()});
     }
 
     public render() {
         return (
-            <div>
+            <div onKeyDown={this.handleOnKeyDown} tabIndex={0}>
                 <p>Representerar ett tangentbord!</p>
                 <p>Grid Container</p>
-                <div className='grid-container' onClick={this.handleOnClick}>
-                    {this.createKeyboard()}
+                <div className='grid-container' onClick={this.handleOnClick} >
+                    {this.state.keyboard}
                 </div>
+                {this.state.input}
             </div>
         )
     }
 
+    private handleOnKeyDown = (event: any): void => {
+        console.log(event.key);
+    }
+
     private handleOnClick = (event: any): void => {
         if (event.target.dataset.value !== undefined) {
-            console.log(event.target.dataset.value);
+            const key = event.target.dataset.value;
+            console.log(key);
+
+            switch (key) {
+                case 'capsLock':
+                    this.setState({isCapsLock: !this.state.isCapsLock}, () => {
+                        this.setState({keyboard: this.createKeyboard()});
+                    });
+                    break;
+                case 'shift':
+                    this.setState({isShift: !this.state.isShift}, () => {
+                        this.setState({keyboard: this.createKeyboard()});
+                    });
+                    break;
+                case 'space':
+                    this.setState((prevState): any => ({
+                        input: [...prevState.input, ' ']
+                    }));
+                    break;
+                case 'back':
+                    
+                    break;
+                case 'enter':
+                    this.setState((prevState): any => ({
+                        input: [...prevState.input, '\n']
+                    }));
+                    break;
+                default:
+                    if (this.state.isShift) {
+                        this.setState({isShift: false}, () => {
+                            this.setState({keyboard: this.createKeyboard()});
+                        });
+                    }
+                    this.setState((prevState): any => ({
+                        input: [...prevState.input, key]
+                    }));
+                    break;
+            }
         }
     }
 
-    private createKeyboard = (): any[] => {
+    private createKeyboard = (): object[] => {
+        console.log('gets called')
         const keys = [
             {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '0', 11: 'back'},
             {1: 'Q', 2: 'W', 3: 'E', 4: 'R', 5: 'T', 6: 'Y', 7: 'U', 8: 'I', 9: 'O', 10: 'P', 11: 'enter'},
@@ -44,7 +94,7 @@ class Keyboard extends React.Component<{}, IKeyboardState> {
             {1: 'shift', 2: 'Z', 3: 'X', 4: 'C', 5: 'V', 6: 'B', 7: 'N', 8: 'M', 9: 'space', 10: '', 11: ''},
         ];
 
-        const keyboard: any = [];
+        const keyboard: object[] = [];
 
         keys.forEach(row => {
             for (const value of Object.keys(row)) {
@@ -58,7 +108,7 @@ class Keyboard extends React.Component<{}, IKeyboardState> {
                 } else if (row[value].length === 0) {
                     console.log('empty');
                 } else {
-                    key = <Key key={row[value]} value={row[value]} type={'letters'}/>
+                    key = <Key key={row[value]} value={(this.state.isCapsLock || this.state.isShift ? row[value] : row[value].toLowerCase())} type={'letters'}/>
                 }
                 keyboard.push(key);
             }
